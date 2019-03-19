fb41f6d64a5a74fa7f40c80d3603b9f331996088
Navin <navin007.a@gmail.com>
2019-03-19 09:07:48 +0000

    Got value from form and displayed in the list

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+    // posts = [
+    //     { title: "First Post", content: "This is the First post's Content"},
+    //     { title: "Second Post", content: "This is the Second post's Content"},
+    //     { title: "Third Post", content: "This is the Third post's Content"}
+    // ];
+    
+    @Input() posts = [];
+    
+}
